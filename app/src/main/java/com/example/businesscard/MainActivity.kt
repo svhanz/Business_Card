414681b0d353536d8a7ace87cc3b0cfdd2c26b8e
Sergei Shlegel <serg-69@mail.ru>
2023-11-01 21:07:45 +0300

    Added logo and test's code

--- app/src/main/java/com/example/businesscard/MainActivity.kt
+++ app/src/main/java/com/example/businesscard/MainActivity.kt
@@ -6,15 +6,20 @@ import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.border
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.painter.Painter
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.Dp
 import androidx.compose.ui.unit.dp
@@ -40,19 +45,31 @@ class MainActivity : ComponentActivity() {
 
 @Composable
 fun AboutMeInfo(logo: Painter, name: String, surname: String, modifier: Modifier = Modifier) {
-	Column {
+	Column(
+		modifier = modifier
+			.fillMaxSize()
+			.border(width = 5.dp, color = Color.Red),
+		verticalArrangement = Arrangement.Center,
+		horizontalAlignment = Alignment.CenterHorizontally
+	){
 		Image(
 			painter = logo,
-			contentDescription = null
+			contentDescription = null,
+			modifier = Modifier
+				.border(width = 5.dp, color = Color.Green)
+				.size(width = 100.dp, height = 100.dp)
 		)
 		Text(
 			text = name,
-			modifier = modifier
+			modifier = Modifier
 				.border(width = 5.dp, color = Color.Blue)
+				.padding(8.dp)
 		)
 		Text(
 			text = surname,
-			modifier = modifier
+			modifier = Modifier
+			.border(width = 5.dp, color = Color.Blue)
+			.padding(8.dp)
 		)
 	}
 }
@@ -61,6 +78,10 @@ fun AboutMeInfo(logo: Painter, name: String, surname: String, modifier: Modifier
 @Composable
 fun GreetingPreview() {
 	BusinessCardTheme {
-		//Greeting("Android")
+		AboutMeInfo(
+			logo = painterResource(id = R.drawable.android_logo),
+			name = "Seg",
+			surname = "Shlegel"
+		)
 	}
 }
