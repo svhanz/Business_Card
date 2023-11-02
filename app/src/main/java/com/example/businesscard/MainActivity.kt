3be61e7f1fd5304d295ff76349d3fa1e71206602
Sergei Shlegel <serg-69@mail.ru>
2023-11-02 21:44:47 +0300

    Finished Project

--- app/src/main/java/com/example/businesscard/MainActivity.kt
+++ app/src/main/java/com/example/businesscard/MainActivity.kt
@@ -1,16 +1,21 @@
 package com.example.businesscard
 
-import android.graphics.drawable.Icon
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.Image
-import androidx.compose.foundation.border
+import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Email
+import androidx.compose.material.icons.filled.Phone
+import androidx.compose.material.icons.filled.Share
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
@@ -19,9 +24,12 @@ import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.painter.Painter
+import androidx.compose.ui.graphics.vector.ImageVector
 import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
-import androidx.compose.ui.unit.Dp
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import com.example.businesscard.ui.theme.BusinessCardTheme
@@ -36,7 +44,7 @@ class MainActivity : ComponentActivity() {
 					modifier = Modifier.fillMaxSize(),
 					color = MaterialTheme.colorScheme.background
 				) {
-					//Greeting("Android")
+					BusinessCardApp()
 				}
 			}
 		}
@@ -44,32 +52,107 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun AboutMeInfo(logo: Painter, name: String, surname: String, modifier: Modifier = Modifier) {
-	Column(
-		modifier = modifier
+fun BusinessCardApp(){
+	AboutMeInfo(
+		logo = painterResource(id = R.drawable.android_logo),
+		fullname = stringResource(R.string.full_name),
+		title = stringResource(R.string.title)
+	)
+}
+@Composable
+fun AboutMeInfo(
+	logo: Painter,
+	fullname: String,
+	title: String,
+	) {
+	Column (
+		modifier = Modifier
 			.fillMaxSize()
-			.border(width = 5.dp, color = Color.Red),
-		verticalArrangement = Arrangement.Center,
+			.background(Color(0xFF494155)),
+		verticalArrangement = Arrangement.SpaceEvenly,
 		horizontalAlignment = Alignment.CenterHorizontally
-	){
-		Image(
-			painter = logo,
+		){
+		Column(
+			modifier = Modifier
+				.fillMaxWidth()
+				.weight(1f)
+				.padding(8.dp),
+			horizontalAlignment = Alignment.CenterHorizontally,
+			verticalArrangement = Arrangement.Center
+		) {
+			Image(
+				painter = logo,
+				contentDescription = null,
+				modifier = Modifier
+					.padding(top = 5.dp)
+					.size(width = 150.dp, height = 150.dp)
+			)
+			Text(
+				text = fullname,
+				modifier = Modifier
+					.padding(8.dp),
+				textAlign = TextAlign.Center,
+				fontSize = 36.sp,
+				fontWeight = FontWeight.Light,
+				color = Color.White
+			)
+			Text(
+				text = title,
+				modifier = Modifier
+					.padding(8.dp),
+				color = Color(0xFF3ddc84),
+				fontSize = 24.sp,
+				fontWeight = FontWeight.Bold
+			)
+		}
+		Column(
+			modifier = Modifier
+				.fillMaxWidth()
+				.weight(0.4f)
+				.padding(10.dp),
+			verticalArrangement = Arrangement.Center,
+			horizontalAlignment = Alignment.End
+		) {
+			ContactInfoTemplate(
+				icon = Icons.Default.Phone ,
+				contact = stringResource(R.string.phone_num)
+			)
+			ContactInfoTemplate(
+				icon = Icons.Default.Share ,
+				contact = stringResource(R.string.home_page)
+			)
+			ContactInfoTemplate(
+				icon = Icons.Default.Email,
+				contact = stringResource(R.string.email)
+			)
+		}
+	}
+}
+
+@Composable
+fun ContactInfoTemplate(
+	icon: ImageVector,
+	contact: String
+) {
+	Row(
+		modifier = Modifier
+			.fillMaxWidth()
+			.padding(start = 70.dp),
+		verticalAlignment = Alignment.CenterVertically,
+		horizontalArrangement = Arrangement.Start
+	) {
+		androidx.compose.material3.Icon(
+			imageVector = icon,
 			contentDescription = null,
+			tint = Color(0xFF3ddc84),
 			modifier = Modifier
-				.border(width = 5.dp, color = Color.Green)
-				.size(width = 100.dp, height = 100.dp)
+				.padding(start = 10.dp, end = 20.dp)
 		)
 		Text(
-			text = name,
-			modifier = Modifier
-				.border(width = 5.dp, color = Color.Blue)
-				.padding(8.dp)
-		)
-		Text(
-			text = surname,
-			modifier = Modifier
-			.border(width = 5.dp, color = Color.Blue)
-			.padding(8.dp)
+			text = contact,
+			color = Color.White,
+			fontSize = 18.sp,
+			modifier = Modifier.padding(bottom = 8.dp),
 		)
 	}
 }
@@ -78,10 +161,6 @@ fun AboutMeInfo(logo: Painter, name: String, surname: String, modifier: Modifier
 @Composable
 fun GreetingPreview() {
 	BusinessCardTheme {
-		AboutMeInfo(
-			logo = painterResource(id = R.drawable.android_logo),
-			name = "Seg",
-			surname = "Shlegel"
-		)
+		BusinessCardApp()
 	}
 }
